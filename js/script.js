42ec3b2b7a53df49e88395bfe5c8b657cb3c909d
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2021-05-18 22:13:16 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+{
+  const tasks = [
+    {
+      content: "nagrać lekcję",
+      done: false,
+    },
+    {
+      content: "zrobić zakupy",
+      done: true,
+    },
+  ];
+
+  init();
+
+  function init() {
+    render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
+  }
+  function onFormSubmit(event) {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-input").value.trim();
+
+    pushTasks(newTaskContent);
+    render();
+  }
+  function pushTasks(newTaskContent) {
+    if (newTaskContent === "") {
+      return;
+    }
+    tasks.push({
+      content: newTaskContent,
+      done: true,
+    });
+  }
+  function render() {
+    let htmlString = "";
+    for (const task of tasks) {
+      htmlString += `<li 
+      ${task.done ? ' style="text-decoration: line-through"' : ""}> 
+      <button class="js-remove"> usuń </button>
+      ${task.content}</li>`;
+    }
+}
+}
